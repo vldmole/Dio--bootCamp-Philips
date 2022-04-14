3b571d6bd46b3320009bd7b2b2697910d635a67e
Vilson Luiz Dalle Mole <vldmole@gmai.com>
2022-04-14 20:11:32 -0300

    removed extra files

--- Nova pasta/modulesJS/main.mjs
+++
@@ -1,5 +0,0 @@
-import { showAge, showHobby, showCity } from './funcoes.mjs'
-
-console.log(showAge('Vilson', 50));
-console.log(showCity('Vilson', 'Toledo'));
-console.log(showHobby('Vilson', 'Code'));
