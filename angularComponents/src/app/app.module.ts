a8a5c462be671d007467e5ddfc435dc85d3a70f9
Vilson Luiz Dalle Mole <vldmole@gmai.com>
2022-05-04 09:15:25 -0300

    "root component moved"

--- angularComponents/src/app/app.module.ts
+++ angularComponents/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponentsModule } from './components/app-components.module';
+
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './rootComponent/app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppComponentsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
